85e3e8ce5e94cb59882287da149430bcd8080f74
Wilco Fiers <wilco@>
2016-04-02 18:27:07 +0200

    insert handlebars

--- build/index.js
+++ build/index.js
@@ -1,31 +1,41 @@
 
-// var readRecursively = require('fs-readdir-recursive');
+var readDir = require('./read-dir-recursive');
 var marked = require('marked');
+var Handlebars = require('handlebars');
 
 var fs = require('fs-extra');
 var path = require('path');
 
-var specDir = path.resolve(__dirname, '../specs/');
+var pagesDir = path.resolve(__dirname, '../pages/');
+var rulesDir = path.resolve(__dirname, '../rules/');
 var outDir = path.resolve(__dirname, '../dist/');
 var templatePath = path.resolve(__dirname, './spec-template.html');
+var tmpRuleDir = path.join(pagesDir, './rules/');
+
+// Empty the output directory
+fs.emptydirSync(outDir);
+
+// Copy rules to the pages dir. We'll remove them again below
+fs.copySync(rulesDir, tmpRuleDir);
+
+// Load the handlebars template
 var template = fs.readFileSync(templatePath, 'utf-8');
 
-// var specFiles = readRecursively(specDir);
-var specFiles = [
-	'/SC1-1-1-aria-describedby.md',
-	'/SC1-1-1-css-image.md',
-];
+var pages = readDir(pagesDir);
+pages.forEach(function (pagePath) {
+	var markdownText = fs.readFileSync(pagePath, 'utf-8');
+	var htmlText = marked(markdownText);
 
-specFiles.forEach(function (fileName) {
-	var specMd = fs.readFileSync(specDir + fileName, 'utf-8');
-	var specHtml = marked(specMd);
+	var html = template.replace('{{content}}', htmlText);
+	var compiledTemplate = Handlebars.compile(html);
+	html = compiledTemplate({
+		'title': path.basename(pagePath, '.md')
+	});
 
-	var html = template
-	.replace('<!-- title -->', path.basename(fileName, '.md'))
-	.replace('<!-- spec -->', specHtml);
-
-	var htmlPath = path.join(outDir, fileName.replace('.md', '.html'));
-	fs.outputFileSync(htmlPath, html);
-	console.log('created file at', htmlPath);
-
+	var outPath = pagePath.replace(pagesDir, outDir).replace('.md', '.html');
+	fs.outputFileSync(outPath, html);
+	console.log('created file at', outPath);
 });
+
+// And remove the tmp rule directory
+fs.removeSync(tmpRuleDir);

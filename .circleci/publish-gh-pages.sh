e616fda7f72e75ba5a5ad1bdacce09dd548a3a80
jkodu <jey.nandakumar@gmail.com>
2019-01-04 13:34:53 -0500

    chore: adding circleci

---
+++ .circleci/publish-gh-pages.sh
@@ -0,0 +1,24 @@
+# vars
+pushToBranch = test-deploy
+
+# set up git config
+git config user.name "$USER_NAME"
+git config user.email "$USER_EMAIL"
+
+# checkout & pull gh-pages branch
+git checkout $pushToBranch
+git pull origin $pushToBranch
+
+# remove all files except the generated site directory and required git folders
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+
+# move generated site in the root folder and remove the empty generated site folder
+mv _site/* .
+rm -R _site/
+
+# commit and push
+git add -fA
+git commit --allow-empty -m "$(git log develop -1 --pretty=%B)"
+git push origin $pushToBranch
+
+echo "deployed successfully"

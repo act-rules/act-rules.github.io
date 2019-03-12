51be41be7d1327b622f731f717761b4884633242
jkodu <jey.nandakumar@gmail.com>
2019-03-12 17:50:55 +0000

    chore: update home page resolution

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,6 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { Redirect } from '@reach/router'
 
-import Layout from "../components/layout/"
-import SEO from "../components/seo"
-
-export default ({ data }) => (
-  <Layout
-  >
-    <SEO
-      title="Home"
-      keywords={[`gatsby`, `application`, `react`]}
-    />
-    <p>Welcome to your new Gatsby site.</p>
-
-    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-    {data.allMarkdownRemark.edges.map(({ node }) => (
-      <div key={node.id}>
-        <Link to={node.fields.slug}>
-          <h3>
-            {node.frontmatter.name}{" "}
-          </h3>
-          <p>{node.excerpt}</p>
-        </Link>
-      </div>
-    ))}
-  </Layout>
+export default () => (
+  <Redirect noThrow to="/pages/about" />
 )
-
-export const query = graphql`
-  query {
-    allMarkdownRemark {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            name
-          }
-          fields {            
-            slug          
-          }          
-          excerpt
-        }
-      }
-    }
-  }
-`

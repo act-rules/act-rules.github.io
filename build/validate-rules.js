abb49f84928afb0603c661932fa0e2f7402fef8d
jkodu <jey.nandakumar@gmail.com>
2019-05-09 16:26:17 +0100

    update validation

--- build/validate-rules.js
+++ build/validate-rules.js
@@ -3,64 +3,93 @@ const path = require('path');
 const globby = require('globby');
 const fastmatter = require('fastmatter');
 
-const isArraySubset = (parent, child) => {
+const exit = (msg, code) => {
+  console[code > 0 ? 'error' : 'info'](msg)
+  process.exit(code)
+}
+
+const isChildArraySubsetOfParent = (parent, child) => {
   return child.every(c => parent.includes(c));
 }
 
-const validateFrontMatter = (frontmatter, filePath) => {
+const validateFrontmatter = (frontmatter, filePath) => {
   const filename = path.parse(filePath).base;
-  const compulsoryKeys = [`id`, `name`, `rule_type`, `description`, `accessibility_requirements`, `authors`]
+  const compulsoryKeys = [
+    `id`,
+    `name`,
+    `rule_type`,
+    `description`,
+    `accessibility_requirements`,
+    `authors`
+  ]
   const keys = Object.keys(frontmatter)
 
   /**
    * 1) Check if all `compulsory` keys exist
    */
-  if (!isArraySubset(keys, compulsoryKeys)) {
-    throw new Error(`One of the compulsory frontmatter attributes - 'id', 'name', 'rule_type', 'description', 'accessibility_requirements', 'authors', is missing in file - ${filePath}.`)
+  if (!isChildArraySubsetOfParent(keys, compulsoryKeys)) {
+    exit(
+      `Rule - ${filename}, at least one of the compulsory frontmatter attributes - 'id', 'name', 'rule_type', 'description', 'accessibility_requirements', 'authors'.`,
+      1
+    )
   }
 
+
+  const { rule_type } = frontmatter
 
   /**
    * 2) check for `input_rules` for `composite` rules
    */
-  const { rule_type, input_rules } = frontmatter
   if (rule_type.toLowerCase() === `composite`) {
-
+    const { input_rules = null } = frontmatter
+    if (!input_rules) {
+      exit(
+        `Rule - ${filename}, is of type "composite" & is missing "input_rules" attribute in frontmatter.`,
+        1
+      )
+    }
   }
 
   /**
-   * check for `input_aspects` for `atomic` rules
+   * 3) check for `input_aspects` for `atomic` rules
    */
+  if (rule_type.toLowerCase() === `atomic`) {
+    const { input_aspects = null } = frontmatter
+    if (!input_aspects) {
+      exit(
+        `Rule - ${filename}, is of type "composite" & is missing "input_aspects" attribute in frontmatter.`,
+        1
+      )
+    }
+  }
 }
 
 const validate = async () => {
-  const errors = []
-
   const rulesFilesPaths = await globby(['./_rules/*.md'])
 
   if (!rulesFilesPaths || !rulesFilesPaths.length) {
-    console.error()
+    exit(
+      `No rules to validate.`,
+      1
+    )
   }
 
-  
+  rulesFilesPaths
+    .forEach(filePath => {
+      const fileContents = fs.readFileSync(filePath, { encoding: 'utf-8' });
+      const { attributes: frontmatter } = fastmatter(fileContents);
 
-  rulesFilesPaths.forEach(filePath => {
-    const fileContents = fs.readFileSync(filePath, { encoding: 'utf-8' });
-    const { attributes: frontmatter } = fastmatter(fileContents);
-
-    /**
-     * 1) validate `frontmatter`
-     */
-    validateFrontMatter(frontmatter, filePath)
-  })
+      /**
+       * 1) validate `frontmatter`
+       */
+      validateFrontmatter(frontmatter, filePath)
+    })
 }
 
 /**
  * init
  */
 (async () => {
-
-  process.exit(1);
-  // const isValid = await validate()
-
+  await validate()
+  exit(`Success`, 0)
 })()

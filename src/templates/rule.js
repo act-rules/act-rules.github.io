ffecc7a96e955369774e46dbf9eea83a493202f1
Jey <jey.nandakumar@gmail.com>
2019-06-07 07:24:50 -0400

    chore: update rule format (#575)

--- src/templates/rule.js
+++ src/templates/rule.js
@@ -3,111 +3,33 @@ import Layout from '../components/layout/'
 import { graphql } from 'gatsby'
 import showdown from 'showdown'
 import {
+	getChangelog,
+	getChangelogLink,
+	getGlossaryUsed,
+	getGlossaryUsedLink,
+	getRuleType,
 	getAccessibilityRequirements,
 	getAuthors,
 	getInputRulesForRule,
 	getInputAspects,
 } from './../utils/render-fragments'
-import glossaryUsages from './../../_data/glossary-usages.json'
 import SEO from '../components/seo'
+import { contributors, repository, config } from './../../package.json'
+
 
 export default ({ data }) => {
 	const { rule, allRules, allGlossary, site } = data
 	const { html, frontmatter, tableOfContents, fields } = rule
-	const { slug, fastmatterAttributes } = fields
+	const { slug, fastmatterAttributes, changelog, fileName } = fields
+	const { relativePath } = fileName
+	const ruleChangelog = JSON.parse(changelog)
 	const { accessibility_requirements } = JSON.parse(fastmatterAttributes)
 	const converter = new showdown.Converter()
 	const updatedTitle = `Rule | ${frontmatter.name} | ${site.siteMetadata.title}`
-	const ruleId = slug.replace('rules/', '')
+	const ruleId = frontmatter.id
 	const ruleTestcasesUrl = `/testcases/${ruleId}/rule-${ruleId}-testcases-for-em-report-tool.json`
-
-	const getRuleType = rule_type => {
-		if (!rule_type) {
-			return null
-		}
-		return (
-			<li>
-				<span role="heading" aria-level="1" className="heading">
-					Rule Type
-				</span>
-				<p>{rule_type}</p>
-			</li>
-		)
-	}
-
-	const getGlossaryItemsUsedInRule = slug => {
-		const keys = []
-		Object.keys(glossaryUsages).forEach(key => {
-			glossaryUsages[key].forEach(({ slug: s }) => {
-				if (s === slug && !keys.includes(key)) {
-					keys.push(key)
-				}
-			})
-		})
-		return keys
-	}
-
-	const renderGlossaryUsed = slug => {
-		const usedKeys = getGlossaryItemsUsedInRule(slug)
-		if (!usedKeys) {
-			return null
-		}
-		const glossaries = allGlossary.edges.filter(({ node }) => {
-			const {
-				frontmatter: { key },
-			} = node
-			return usedKeys.includes(`#${key}`)
-		})
-		if (!glossaries.length) {
-			return null
-		}
-		return (
-			<>
-				<br />
-				<hr />
-				<a id="glossary-listing" href="#glossary-listing">
-					<h2>Referenced Glossary</h2>
-				</a>
-				{glossaries.map(({ node }) => {
-					const { frontmatter, html } = node
-					const { key } = frontmatter
-					return (
-						<article key={node.id}>
-							<a id={key} href={`#${key}`}>
-								<h3>{frontmatter.title}</h3>
-							</a>
-							<i>
-								key: <u>{key}</u>
-							</i>
-							<div dangerouslySetInnerHTML={{ __html: html }} />
-							<br />
-						</article>
-					)
-				})}
-			</>
-		)
-	}
-
-	const renderGlossaryUsedLink = slug => {
-		const usedKeys = getGlossaryItemsUsedInRule(slug)
-		if (!usedKeys) {
-			return null
-		}
-		const glossaries = allGlossary.edges.filter(({ node }) => {
-			const {
-				frontmatter: { key },
-			} = node
-			return usedKeys.includes(`#${key}`)
-		})
-		if (!glossaries.length) {
-			return null
-		}
-		return (
-			<li>
-				<a href="#glossary-listing">Referenced Glossary</a>
-			</li>
-		)
-	}
+	const issuesUrl = `${repository.url}/issues?q=${ruleId}`
+	const ruleFormatInputAspects = config['rule-format-metadata']['input-aspects']
 
 	return (
 		<Layout>
@@ -123,7 +45,7 @@ export default ({ data }) => {
 					<ul className="meta">
 						{getRuleType(frontmatter.rule_type)}
 						<li>{getAccessibilityRequirements(accessibility_requirements)}</li>
-						<li>{getInputAspects(frontmatter.input_aspects)}</li>
+						<li>{getInputAspects(frontmatter.input_aspects, ruleFormatInputAspects)}</li>
 						<li>
 							{getInputRulesForRule(
 								frontmatter.input_rules,
@@ -131,11 +53,9 @@ export default ({ data }) => {
 								true
 							)}
 						</li>
-						<li>{getAuthors(frontmatter.authors)}</li>
 					</ul>
-
+					<hr />
 					{/* Description */}
-					<br />
 					<div
 						dangerouslySetInnerHTML={{
 							__html: converter.makeHtml(frontmatter.description),
@@ -147,18 +67,38 @@ export default ({ data }) => {
 							__html: html,
 						}}
 					/>
+					<hr />
 					{/* glossary */}
-					{renderGlossaryUsed(slug)}
+					{getGlossaryUsed(slug, allGlossary)}
+					<hr />
+					{/* changelog */}
+					{getChangelog(ruleChangelog, repository.url, `_rules/${relativePath}`)}
+					{/* acknowledgements */}
+					<hr />
+					<a id="acknowledgements" href="#acknowledgements">
+						<h2>Acknowledgements</h2>
+					</a>
+					<div className="meta">
+						{getAuthors(frontmatter.authors, contributors)}
+					</div>
 				</section>
 				{/* Toc */}
 				<div className="toc">
+					{/* todo:jey needs fixing up */}
 					<span role="heading" aria-level="1" className="heading">
 						Table of Contents
 					</span>
 					<div dangerouslySetInnerHTML={{ __html: tableOfContents }} />
-					<ul>{renderGlossaryUsedLink(slug)}</ul>
+					<ul>
+						{getGlossaryUsedLink(slug, allGlossary)}
+						{getChangelogLink(ruleChangelog)}
+						<li>
+							<a href="#acknowledgements">Acknowledgements</a>
+						</li>
+					</ul>
+					{/* todo:jey needs fixing up */}
 					<span role="heading" aria-level="1" className="heading">
-						Download Testcases
+						Useful Links
 					</span>
 					<ul>
 						<li>
@@ -167,9 +107,20 @@ export default ({ data }) => {
 								aria-label="test cases of rule for use in wcag em report tool"
 								target="_blank"
 								rel="noopener noreferrer"
+								href={issuesUrl}
+							>
+								View Issues
+							</a>
+						</li>
+						<li>
+							<a
+								className="btn-secondary"
+								aria-label="test cases of rule for use in wcag em report tool"
+								target="_blank"
+								rel="noopener noreferrer"
 								href={ruleTestcasesUrl}
 							>
-								For EM Report Tool
+								Testcases (EM Report Tool)
 							</a>
 						</li>
 					</ul>
@@ -186,6 +137,7 @@ export const query = graphql`
 			tableOfContents
 			fileAbsolutePath
 			frontmatter {
+				id
 				name
 				rule_type
 				description
@@ -194,8 +146,12 @@ export const query = graphql`
 				authors
 			}
 			fields {
+				fileName {
+          relativePath
+        }
 				slug
 				fastmatterAttributes
+				changelog
 			}
 		}
 		allRules: allMarkdownRemark(

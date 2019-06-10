a998e941ac9436932fe041ff7eda81cde27bdc9c
jkodu <jey.nandakumar@gmail.com>
2019-06-10 19:53:32 +0100

    update

--- implementations/axe-core/main.js
+++ implementations/axe-core/main.js
@@ -11,7 +11,7 @@ const start = +args[0] || 0
 const size = +args[1] || (start ? 1 : undefined)
 const options = { start, size }
 
-async function main (runTC = runTestCases) {
+async function main(runTC = runTestCases) {
   const browser = await puppeteer.launch()
   let page = await browser.newPage()
   await page.setBypassCSP(true)
@@ -21,19 +21,16 @@ async function main (runTC = runTestCases) {
   })
   await page.close()
   await browser.close()
-  
+
   // Save EARL
   const earlResults = concatReport(testResults)
   const earlPath = path.resolve(__dirname, './axe-earl-results.json')
   await writeFile(earlPath, JSON.stringify(earlResults, null, 2), 'utf-8')
-  console.log(`Saved report to ${earlPath}`)
+  // console.log(`Saved report to ${earlPath}`)
 }
 
-if (!module.parent) {
-  module.exports.default = main
-} else {
-  main().catch(e => {
-    console.error(e)
-    process.exit(1)
-  })
-}
+main()
+.catch(e => {
+  console.error(e)
+  process.exit(1)
+})

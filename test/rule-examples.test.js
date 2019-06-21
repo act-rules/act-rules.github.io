01a9570b90db27160875d23dafe36efc54f2385f
jkodu <jey.nandakumar@gmail.com>
2019-06-21 16:20:42 +0100

    add more logging

--- test/rule-examples.test.js
+++ test/rule-examples.test.js
@@ -1,23 +1,35 @@
 const codeBlocks = require('gfm-code-blocks')
+const describeRule = require('./utils/describe-rule')
 /**
- * See 
+ * See
  * - https://html-validate.org/
  * - https://html-validate.org/rules/
  */
 const { HtmlValidate } = require("html-validate");
-const describeRule = require('./utils/describe-rule')
-
+const { getFormatter } = require("html-validate/build/cli/formatter");
+const stylish = getFormatter("stylish");
 const htmlValidate = new HtmlValidate({
   extends: ["htmlvalidate:recommended"],
 });
 
 describeRule('examples', (ruleData) => {
-  const { body } = ruleData
-  const codeSnippets = codeBlocks(body)
-  test.each(codeSnippets)('has valid HTML code snippet = `%j`',
-    snippet => {
-      const { code } = snippet;
-      const { valid } = htmlValidate.validateString(code);
-      expect(valid).toBe(true)
-    })
+  const { frontmatter, body } = ruleData
+  const { id, name } = frontmatter
+  const codeSnippets = codeBlocks(body).map(block => block.code)
+  test.each(codeSnippets)('is valid HTML code - `%s`', snippet => {
+    const report = htmlValidate.validateString(snippet);
+    if (!report.valid) {
+      console.log(`Rule Name: ${name} \n`);
+      console.log(`Rule Id: ${id} \n`);
+      // console.log(JSON.stringify(report.results, undefined, 2));
+
+      report.results.forEach(({ source, messages }) => {
+        console.log(`Source: \n`, source);
+        console.table(messages);
+      });
+
+
+    }
+    expect(report.valid).toBe(true);
+  })
 })

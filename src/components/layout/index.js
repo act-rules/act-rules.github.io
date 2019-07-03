744096f0a7898c969ca75fb620b2516963705481
jkodu <jey.nandakumar@gmail.com>
2019-07-03 15:39:40 +0100

    update

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,12 +1,13 @@
-import 'normalize.css'
-import './index.scss'
-
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql, Link } from 'gatsby'
+import ReactMedia from 'react-media'
 
 import Header from '../header/index'
 import Footer from '../footer'
+
+import 'normalize.css'
+import './index.scss'
 
 class Layout extends React.Component {
 	constructor(props) {
@@ -104,6 +105,12 @@ class Layout extends React.Component {
 					} = data
 					return (
 						<section className="layout-container">
+							{/* hide menu when width <= 600px */}
+							<ReactMedia query="(max-width: 600px)"
+								onChange={matches => (matches && this.state.showMenu) && this.handleHideShowMenu()} />
+							{/* show menu when width > 600px */}
+							<ReactMedia query="(min-width: 601px)"
+								onChange={matches => (matches && !this.state.showMenu) && this.handleHideShowMenu()} />
 							<aside className={this.state.showMenu ? 'show' : 'hide'}>
 								<button
 									className="nav-hide-show-menu"

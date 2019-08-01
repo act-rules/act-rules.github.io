1e3552cd8793658778ca8f207ae75d078adf17fe
jkodu <jey.nandakumar@gmail.com>
2019-08-01 14:36:28 +0100

    chore: run prettier

--- _rules/__tests__/testcases.js
+++ _rules/__tests__/testcases.js
@@ -4,42 +4,42 @@ const prettyJson = require('prettyjson')
 const describeRule = require('../../test-utils/describe-rule')
 
 describeRule('testcases', ruleData => {
-  const { frontmatter, body } = ruleData
-  const { id, name } = frontmatter
+	const { frontmatter, body } = ruleData
+	const { id, name } = frontmatter
 
-  /**
-   * get test case code snippets of the rule
-   */
-  const codeSnippets = codeBlocks(body).map(({ code }) => code)
+	/**
+	 * get test case code snippets of the rule
+	 */
+	const codeSnippets = codeBlocks(body).map(({ code }) => code)
 
-  /**
-   * Check if testcases are valid
-   * - using `html-validator`
-   * - check fragment `isFragment = true`
-   */
-  test.each(codeSnippets)('is valid testcase: `%p`', async (snippet) => {
-    const options = {
-      data: snippet,
-      isFragment: true,
-      format: 'json'
-    }
-    const result = await validator(options)
+	/**
+	 * Check if testcases are valid
+	 * - using `html-validator`
+	 * - check fragment `isFragment = true`
+	 */
+	test.each(codeSnippets)('is valid testcase: `%p`', async snippet => {
+		const options = {
+			data: snippet,
+			isFragment: true,
+			format: 'json',
+		}
+		const result = await validator(options)
 
-    expect(result).toBeObject()
-    expect(result).toContainKey('messages')
+		expect(result).toBeObject()
+		expect(result).toContainKey('messages')
 
-    const { messages } = result
-    const errorMessages = messages.filter(({ type }) => type === `error`)
+		const { messages } = result
+		const errorMessages = messages.filter(({ type }) => type === `error`)
 
-    if (errorMessages.length) {
-      const output = {
-        "Rule Id": id,
-        "Rule Name": name,
-        "Snippet": snippet,
-        "Errors": errorMessages.map(({message}) => message),
-      };
-      console.log(prettyJson.render(output));
-    }
-    expect(errorMessages.length).toBe(0)
-  })
+		if (errorMessages.length) {
+			const output = {
+				'Rule Id': id,
+				'Rule Name': name,
+				Snippet: snippet,
+				Errors: errorMessages.map(({ message }) => message),
+			}
+			console.log(prettyJson.render(output))
+		}
+		expect(errorMessages.length).toBe(0)
+	})
 })

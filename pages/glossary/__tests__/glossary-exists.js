cbf0f274e11a8c99bea1b67661c2cd90f0bdc4fb
jkodu <jey.nandakumar@gmail.com>
2019-08-29 13:01:39 +0100

    test:  validate terms in glossary markdown

--- pages/glossary/__tests__/glossary-exists.js
+++ pages/glossary/__tests__/glossary-exists.js
@@ -1,21 +1,42 @@
+const { config } = require('../../../package.json')
 const getMarkdownData = require('../../../utils/get-markdown-data')
 const glossariesInRules = require('../../../_data/glossaries-in-rules.json')
+const glossariesInGlossaries = require('../../../_data/glossaries-in-glossaries.json')
 
 const glossariesMarkdownData = getMarkdownData(config.markdown.glossary)
 const glossaryKeys = glossariesMarkdownData.map(({ frontmatter }) => `#${frontmatter.key}`)
 
-describe('glossary terms used should exist', () => {
-
-	keysOfGlossariesInRules = Object.keys(glossariesInRules)
-
-	
-	test.each(keysOfGlossariesInRules)
+/**
+ * Helper fn to run `test.each` on a given iteraable object
+ * @param {Array<String>} glossaries keys of glossary terms
+ */
+const glossariesExists = (glossaries) => {
+	test.each(glossaries)
 		('has glossary term `%s`', key => {
 			const fileExists = glossaryKeys.includes(key)
-			if (!fileExists) {
-				console.log(`glossary missing for ${key}, usages below:`)
-				console.table(glossariesInRules[key])
-			}
-			expect(fileExists).toBe(true)
+			expect(
+				fileExists,
+				`${key} glossary definition missing`
+			).toBe(true)
 		})
+}
+
+describe('Glossary terms should exist', () => {
+
+	describe(`Terms used in rules`, () => {
+		keysOfGlossariesInRules = Object.keys(glossariesInRules)
+		glossariesExists(keysOfGlossariesInRules)
+	})
+
+	describe(`Terms used in glossaries`, () => {
+		keysOfGlossariesInGlossaries = Object.values(glossariesInGlossaries).reduce((out, values) => {
+			for (const { key }  of values) {
+				if (!out.includes(key)) {
+					out.push(key)
+				}
+			}
+			return out
+		}, [])
+		glossariesExists(keysOfGlossariesInGlossaries)
+	})
 })

af93ac269cace6527cc08eead9e6ff23ba443bf2
jkodu <jey.nandakumar@gmail.com>
2019-09-02 12:51:48 +0100

    add more logic to spelling checks

--- __tests__/spelling.js
+++ __tests__/spelling.js
@@ -2,33 +2,57 @@ const retext = require('retext')
 const spell = require('retext-spell')
 const dictionary = require('dictionary-en-gb')
 const reporter = require('vfile-reporter')
+const gfmCodeBlocks = require('gfm-code-blocks')
+const markdownLinkExtractor = require('markdown-link-extractor')
 
 const describeRule = require('../test-utils/describe-rule')
 const describePage = require('../test-utils/describe-page')
 
-const temp = () => {
+const ignoreSpellcheckList = ['SVG', 'DOM', 'href']
+const spellOptions = {
+	dictionary,
+	ignore: ignoreSpellcheckList,
+}
+
+const getCurateMarkdownBody = (body, options) => {
+	const { stripCodeBlocks = false, stripHyperlinks = false } = options
+
+	if (stripCodeBlocks) {
+		const codeBlocks = gfmCodeBlocks(body)
+		body = codeBlocks.reduce((out, { block }) => out.replace(block, ''), body)
+	}
+
+	if (stripHyperlinks) {
+		const hyperlinks = markdownLinkExtractor(body)
+		body = hyperlinks.reduce((out, hyperlink) => out.replace(hyperlink, ''), body)
+	}
+
+	return body
+}
+
+const checkSpelling = text => {
 	return new Promise((resolve, reject) => {
 		retext()
-			.use(spell, dictionary)
-			.process(body, function(err, file) {
+			.use(spell, spellOptions)
+			.process(text, (err, file) => {
 				if (err) {
-					reject(err)
+					reject([false, error])
 				}
-				resolve(file)
-				done()
+				resolve([true])
 			})
 	})
 }
+
+const validateMarkdownBody = async ({ body }) => {}
+
 /**
  * Validate `Rules` and `Pages` for spelling/ typos.
  */
 describePage('Validate text for spelling mistakes', () => {
-	describeRule('Spellcheck Rules', async ruleData => {
-		const { body } = ruleData
-		try {
-			const result = await temp(body)
-		} catch (error) {
-			console.error(reporter(error))
-		}
+	describeRule('spellcheck rules', ruleData => async ({ body }) => {
+		const curatedBody = getCurateMarkdownBody(body, { stripCodeBlocks: true, stripHyperlinks: true })
+		expect.assertions(1)
+		const [result, error] = await checkSpelling(curatedBody)
+		expect(result).toEqual(true)
 	})
 })

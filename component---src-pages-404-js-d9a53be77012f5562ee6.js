dfcc0259aff078b9d7800fcd584d65d64513d5d2
jkodu <jey.nandakumar@gmail.com>
2019-09-19 11:54:21 +0100

    update site [ci skip]

---
+++ component---src-pages-404-js-d9a53be77012f5562ee6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{226:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(218),o=n(217);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d9a53be77012f5562ee6.js.map

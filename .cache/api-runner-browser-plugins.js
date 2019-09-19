758c2db2e9f21e825329d97b55bee2e3009a82df
jkodu <jey.nandakumar@gmail.com>
2019-09-19 15:21:11 +0100

    update site [ci skip]

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/jey/Documents/work/deque/act-rules.github.io/node_modules/gatsby-remark-autolink-headers/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]

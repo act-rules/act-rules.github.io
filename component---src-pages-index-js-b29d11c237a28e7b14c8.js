130c82de866a4142ebe910e86c5d85fa12e0d067
jkodu <jey.nandakumar@gmail.com>
2019-09-23 23:51:24 +0100

    update site [ci skip]

---
+++ component---src-pages-index-js-b29d11c237a28e7b14c8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{228:function(e,n,t){"use strict";t.r(n);var o=t(0),a=t.n(o),r=t(30);n.default=function(){return a.a.createElement(r.Redirect,{noThrow:!0,to:"/pages/about"})}}}]);
+//# sourceMappingURL=component---src-pages-index-js-b29d11c237a28e7b14c8.js.map

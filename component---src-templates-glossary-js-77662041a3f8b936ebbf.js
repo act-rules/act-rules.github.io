9201cc3fb507a6a5d0d9dec3b621412232c15c90
jkodu <jey.nandakumar@gmail.com>
2019-10-29 18:10:04 +0000

    update site from act-website repo

---
+++ component---src-templates-glossary-js-77662041a3f8b936ebbf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{226:function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return c}));var a=n(0),r=n.n(a),l=n(221),u=n(220);t.default=function(e){var t=e.data.markdownRemark,n=t.html,a=t.frontmatter;return r.a.createElement(l.a,null,r.a.createElement(u.a,{title:a.title}),r.a.createElement("div",null,r.a.createElement("h1",null,a.title),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:n}})))};var c="2016649015"}}]);
+//# sourceMappingURL=component---src-templates-glossary-js-77662041a3f8b936ebbf.js.map

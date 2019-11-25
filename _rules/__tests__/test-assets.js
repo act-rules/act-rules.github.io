c64040f643a8c4d62111d170724781e1673520c5
jkodu <jey.nandakumar@gmail.com>
2019-11-25 15:47:21 +0000

    test: add tests to catch non-existent assets

--- _rules/__tests__/test-assets.js
+++ _rules/__tests__/test-assets.js
@@ -1,20 +1,64 @@
+const globby = require('globby')
 const gfmCodeBlocks = require('gfm-code-blocks')
+const isUrl = require('is-url')
+
 const describeRule = require('../../test-utils/describe-rule')
+const getAllMatchesForRegex = require('../../utils/get-all-matches-for-regex')
+const allTestAssetPaths = uniqueArrayItems(globby.sync(`./test-assets/**/*.*`).map(path => path.replace(`./`, ``)))
 
 describeRule('check if referenced test assets exists', ruleData => {
 	const codeBlocks = gfmCodeBlocks(ruleData.body)
+	const usedAssetPaths = getUniqueTestAssetsUsedInCodeBlocks(codeBlocks)
 
-	/**
-	 * Get only code blocks that refer to `test-assets`
-	 */
-	const codeSnippetsReferringTestAssets = codeBlocks.filter(({ block }) => {
-		console.log(block)
-
-		const match = new RegExp(/\s\[[^(]*?\]\r?\n?\(#.*?\)/g).exec(block).match
-		return true
-	})
-
-	test(`temp`, () => {
-		expect(1).toBe(1)
-	})
+	if (usedAssetPaths && usedAssetPaths.length) {
+		test.each(usedAssetPaths)('%s', path => {
+			expect(allTestAssetPaths.includes(path), path).toBe(true)
+		})
+	}
 })
+
+/**
+ * Parse given code blocks in a given rule's markdown data & get test-asset paths
+ * @param {Object[]} codeBlocks code blocks in a give rule markdown data
+ * @returns {String[]}
+ */
+function getUniqueTestAssetsUsedInCodeBlocks(codeBlocks) {
+	const result = []
+	const codeReferencingTestAssets = codeBlocks.filter(({ code }) => /test-assets\//.test(code))
+
+	for (const { code } of codeReferencingTestAssets) {
+		const paths = getAllMatchesForRegex(/(href|src)="[^"]*"/, code, false)
+		for (const path of paths) {
+			const assetPath = curateAssetPath(path.block)
+			if (!isUrl(assetPath)) {
+				/**
+				 * In some places, `test-assets` are referred as `../../test-assets` & sometimes as `./test-assets` etc.
+				 * Normalising all references to `test-assets/...`
+				 */
+				const relativePath = assetPath.slice(assetPath.indexOf('test-assets'))
+				result.push(relativePath)
+			}
+		}
+	}
+
+	return uniqueArrayItems(result)
+}
+
+/**
+ * Curate a given asset path of attribtues and quotes
+ * @param {String} str given asset path
+ */
+function curateAssetPath(str) {
+	return str
+		.replace('src=', '')
+		.replace('href=', '')
+		.replace(/"/g, '')
+}
+
+/**
+ * Helper to unique array of primitive items
+ * @param {Array<String>} items
+ */
+function uniqueArrayItems(items) {
+	return [...new Set(items)]
+}

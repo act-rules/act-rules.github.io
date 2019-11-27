7f9fa250d8d3e77e065fd047fd2628ec4010c9cb
jkodu <jey.nandakumar@gmail.com>
2019-11-27 15:48:35 +0000

    update tests to curate path of assets

--- _rules/__tests__/test-assets.js
+++ _rules/__tests__/test-assets.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const assert = require('assert')
 const globby = require('globby')
 const gfmCodeBlocks = require('gfm-code-blocks')
 const isUrl = require('is-url')
@@ -11,8 +13,9 @@ describeRule('check if referenced test assets exists', ruleData => {
 	const usedAssetPaths = getUniqueTestAssetsUsedInCodeBlocks(codeBlocks)
 
 	if (usedAssetPaths && usedAssetPaths.length) {
-		test.each(usedAssetPaths)('%s', path => {
-			expect(allTestAssetPaths.includes(path), path).toBe(true)
+		test.each(usedAssetPaths)('%s', assetPath => {
+			const message = `Asset at path -> ${assetPath} does not exist`
+			expect(allTestAssetPaths.includes(assetPath), message).toBe(true)
 		})
 	}
 })
@@ -27,32 +30,66 @@ function getUniqueTestAssetsUsedInCodeBlocks(codeBlocks) {
 	const codeReferencingTestAssets = codeBlocks.filter(({ code }) => /test-assets\//.test(code))
 
 	for (const { code } of codeReferencingTestAssets) {
-		const paths = getAllMatchesForRegex(/(href|src)="[^"]*"/, code, false)
-		for (const path of paths) {
-			const assetPath = curateAssetPath(path.block)
-			if (!isUrl(assetPath)) {
-				/**
-				 * In some places, `test-assets` are referred as `../../test-assets` & sometimes as `./test-assets` etc.
-				 * Normalising all references to `test-assets/...`
-				 */
-				const relativePath = assetPath.slice(assetPath.indexOf('test-assets'))
-				result.push(relativePath)
+		const assetPaths = getAllMatchesForRegex(/(href|src)="[^"]*"/, code, false)
+		for (const { block } of assetPaths) {
+			const assetPath = curateAssetPath(block)
+			/**
+			 * Ignore URL
+			 */
+			if (!assetPath || isUrl(assetPath)) {
+				return
 			}
+
+			/**
+			 * In some places, `test-assets` are referred as `../../test-assets` & sometimes as `./test-assets` etc.
+			 * Normalising all references to `test-assets/...`
+			 */
+			assert(
+				assetPath.indexOf('test-assets') >= 0,
+				`Used path -> ${assetPath} is not referencing "test-assets" directory`
+			)
+			const relativePath = assetPath.slice(assetPath.indexOf('test-assets'))
+			result.push(relativePath)
 		}
 	}
 
-	return uniqueArrayItems(result)
+	return uniqueArrayItems(result).filter(item => !!item)
 }
 
 /**
  * Curate a given asset path of attribtues and quotes
  * @param {String} str given asset path
  */
-function curateAssetPath(str) {
-	return str
+function curateAssetPath(assetPath) {
+	/**
+	 * curate given path of
+	 * `src` attribute
+	 * `href` attribute
+	 * `""` quotes
+	 */
+	const newPath = assetPath
 		.replace('src=', '')
 		.replace('href=', '')
 		.replace(/"/g, '')
+
+	/**
+	 * Ignore assets that do not actually refer to a file/asset
+	 * eg: `test-assets/directory` is ok
+	 */
+	const extn = path.extname(newPath)
+	if (!extn) {
+		return undefined
+	}
+
+	/**
+	 * Strip any `#` search params
+	 */
+	const hashIndex = newPath.indexOf('#')
+	if (hashIndex >= 0) {
+		return newPath.replace(newPath.substring(hashIndex), '')
+	}
+
+	return newPath
 }
 
 /**

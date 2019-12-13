d395a7e3a612460e12554bf28c0ca36c27db603b
jkodu <jey.nandakumar@gmail.com>
2019-12-13 15:44:18 +0000

    update site from act-website repo

--- styles-4d024f13c3ad78d1ca01.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"4DHH":function(n,i,o){},"6MIl":function(n,i,o){},"6vEM":function(n,i,o){},"9d8Q":function(n,i,o){},APhT:function(n,i,o){},N3Pj:function(n,i,o){},R6x1:function(n,i,o){},XjQp:function(n,i,o){},Yh6b:function(n,i,o){},"Zi+Q":function(n,i,o){},bvMr:function(n,i,o){},mDvL:function(n,i,o){},sSi1:function(n,i,o){},tfdY:function(n,i,o){},u1WH:function(n,i,o){},wNCM:function(n,i,o){},wcru:function(n,i,o){},wqB8:function(n,i,o){}}]);
-//# sourceMappingURL=styles-4d024f13c3ad78d1ca01.js.map
---
+++ testcases/e6952f/5e7c5010667dba4b3d6d878dfc85147e936adbc8.js
@@ -0,0 +1,1 @@
+var foo = '<img src="image.jpg" alt="" alt="image" />'

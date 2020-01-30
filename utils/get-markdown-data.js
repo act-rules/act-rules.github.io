51035141e6846bc0bbd1aca152b1a12b337ca7ff
jkodu <jey.nandakumar@gmail.com>
2020-01-30 15:26:13 +0000

    rename test for html hint

--- utils/get-markdown-data.js
+++ utils/get-markdown-data.js
@@ -2,23 +2,28 @@ const fs = require('fs')
 const globby = require('globby')
 const path = require('path')
 const fastmatter = require('fastmatter')
-const remark = require('remark')
+const unified = require('unified')
+const remarkParse = require('remark-parse')
+const remarkFrontmatter = require('remark-frontmatter')
 
 /**
  * Parse all markdown files in a given directory and construct metadata of each markdown file
  *
  * @param {String} dir path to directory containing markdown files
- * @param {Array} exclude (Optional) list of paths to exclude
  * @returns {Object}
  */
-const getMarkdownData = (dir, exclude = []) => {
-	return globby.sync([`${dir}/**/*.md`, ...exclude]).map(markdownPath => {
+const getMarkdownData = dir => {
+	return globby.sync(`${dir}/**/*.md`).map(markdownPath => {
 		const filename = path.parse(markdownPath).base
+
 		const fileContents = fs.readFileSync(markdownPath, { encoding: 'utf-8' })
-		const markdownAST = remark.parse(fileContents)
+		const unifiedProcesser = unified()
+			.use(remarkParse)
+			.use(remarkFrontmatter)
+		const markdownAST = unifiedProcesser.parse(fileContents)
+
 		const { attributes: frontmatter, body } = fastmatter(fileContents)
 		return {
-			path: markdownPath,
 			filename,
 			frontmatter,
 			body,

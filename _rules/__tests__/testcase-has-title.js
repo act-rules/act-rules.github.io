a0a4e7f5acfd5953f5011898830a8316eaf9c66d
jkodu <jey.nandakumar@gmail.com>
2020-01-30 15:26:32 +0000

    test: add test to check if all testcase has title

---
+++ _rules/__tests__/testcase-has-title.js
@@ -0,0 +1,34 @@
+const describeRule = require('../../test-utils/describe-rule')
+const getMarkdownAstNodesOfType = require('../../utils/get-markdown-ast-nodes-of-type')
+
+describeRule('testcase has title', ({ filename, markdownAST }) => {
+	/**
+	 * get all titles of test case examples (eg: #### Failed Example 1)
+	 */
+	const testcaseTitles = getMarkdownAstNodesOfType(markdownAST, 'heading')
+		.filter(({ depth, children }) => {
+			return depth === 4 && children && children.length > 0
+		})
+		.map(({ children }) => {
+			const [textNode] = children
+			return textNode.value
+		})
+
+	/**
+	 * get code blocks in markdown body
+	 */
+	const testcaseCodeSnippets = getMarkdownAstNodesOfType(markdownAST, 'code')
+	if (testcaseTitles.length !== testcaseCodeSnippets.length) {
+		throw new Error(``)
+	}
+
+	/**
+	 * Check if filename has `id` as a part of the name
+	 */
+	test('each testcase has a title', () => {
+		const expected = testcaseCodeSnippets.length
+		const actual = testcaseTitles.length
+		const msg = `Not all test cases have titles in ${filename}.`
+		expect(actual, msg).toBe(expected)
+	})
+})

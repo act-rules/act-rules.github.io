f757b6aff7532215e64998c9f373d4506288cbd6
jkodu <jey.nandakumar@gmail.com>
2020-03-25 10:56:14 +0000

    test: check if angle brackets are used around html tags

--- __tests__/html-tag-no-angle-bracket.js
+++ __tests__/html-tag-no-angle-bracket.js
@@ -2,32 +2,70 @@ const htmlTags = require('html-tags')
 const describeRule = require('../test-utils/describe-rule')
 const describePage = require('../test-utils/describe-page')
 const getAllMatchesForRegex = require('../utils/get-all-matches-for-regex')
-
+const getMarkdownAstNodesOfType = require('../utils/get-markdown-ast-nodes-of-type')
 const htmlTagsWithAngleBrackets = htmlTags.map(tag => `<${tag}>`)
+
 describe(`Validate HTML Tags for no '<>' angle brackets`, () => {
 	/**
 	 * Rules, files under `_rules/*.md
 	 */
-	describeRule(`Rules`, ({ markdownAST, frontmatter, body }) => {
+	describeRule(`Rules`, ({ markdownAST, frontmatter }) => {
 		const { name, description } = frontmatter
-		validateHtmlTags(name)
-		validateHtmlTags(description)
-		validateHtmlTags(body)
+
+		/**
+		 * Validate name & description from frontmatter
+		 */
+		testForAngleBracketsAroundTags(getTextSurroundedByAngleBrackets(name))
+		testForAngleBracketsAroundTags(getTextSurroundedByAngleBrackets(description))
+		// get all text, html, and inlineCode used in markdown body and construct as a string to be validated
+		const body = getMarkdownAstNodesOfType(markdownAST, ['text', 'html', 'inlineCode'])
+			.map(({ value }) => value)
+			.join('\n')
+		testForAngleBracketsAroundTags(getTextSurroundedByAngleBrackets(body))
 	})
+
 	/**
 	 * Pages, files under `pages/*.md`
 	 */
+	describeRule(`Pages`, ({ markdownAST, frontmatter }) => {
+		const { title } = frontmatter
+
+		/**
+		 * Validate title
+		 */
+		testForAngleBracketsAroundTags(getTextSurroundedByAngleBrackets(title))
+		// get all text, html, and inlineCode used in markdown body and construct as a string to be validated
+		const body = getMarkdownAstNodesOfType(markdownAST, ['text', 'html', 'inlineCode'])
+			.map(({ value }) => value)
+			.join('\n')
+		testForAngleBracketsAroundTags(getTextSurroundedByAngleBrackets(body))
+	})
 })
 
-function validateHtmlTags(text) {
-	// todo: this needs to not interpret code snippets
-	const matches = getAllMatchesForRegex(/\<(.*?)\>/, text, false)
+function getTextSurroundedByAngleBrackets(body) {
+	if (!body || !body.length) {
+		return []
+	}
+
+	const matches = getAllMatchesForRegex(/\<(.*?)\>/, body, false)
 	if (!matches || !matches.length) {
-		return
+		return []
 	}
 
 	const blocks = matches.map(({ block }) => block)
-	test.each(blocks)('%s', block => {
-		expect(htmlTagsWithAngleBrackets.includes(block)).toBe(false)
-	})
+	return blocks || []
+}
+
+/**
+ * Helper fn to assert a given text for usage of angle brackets
+ * @param {String} text string
+ */
+function testForAngleBracketsAroundTags(items) {
+	if (items.length) {
+		test.each(items)('%s has no <> bracket', item => {
+			const actual = htmlTagsWithAngleBrackets.includes(item)
+			const msg = `${item} uses angle brackets`
+			expect(actual, msg).toBe(false)
+		})
+	}
 }

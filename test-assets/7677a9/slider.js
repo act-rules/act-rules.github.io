48775b30b04cf5392a9af46a32d733e0619ad5aa
carlosapaduarte <carlosapaduarte@gmail.com>
2020-04-08 17:19:02 +0100

    Update slider.js

--- test-assets/7677a9/slider.js
+++ test-assets/7677a9/slider.js
@@ -1,44 +1,42 @@
-function increaseSlider() {
-    document.getElementById('motionSlider').value++;
+function changeSlider(amount) {
+    document.getElementById('motionSlider').value += amount;
     document.getElementById('output').innerHTML = document.getElementById('motionSlider').value;
+}
+
+function increaseSlider() {
+    changeSlider(+1);
 }
 
 function decreaseSlider() {
-    document.getElementById('motionSlider').value--;
-    document.getElementById('output').innerHTML = document.getElementById('motionSlider').value;
+    changeSlider(-1);
+}
+
+function applyChange(gamma, threshold) {
+    if (gamma > threshold) {
+        increaseSlider();
+    } else if (gamma < -threshold) {
+        decreaseSlider();
+    }
 }
 
 function handleOrientation(event) {
-    if (event.gamma > 20) {
-        increaseSlider();
-    } else if (event.gamma < -20) {
-        decreaseSlider();
-    }
+    applyChange(event.gamma, 20);
 }
 
 function handleOrientationCanBeDisabled(event) {
     const disableMotion = document.getElementById('disableMotion');
-    const gamma = disableMotion.checked ? 0 : event.gamma;
-
-    if (gamma > 20) {
-        increaseSlider();
-    } else if (gamma < -20) {
-        decreaseSlider();
+    if (!disableMotion.checked) {
+        applyChange(event.gamma);
     }
 }
 
 function handleMotion(event) {
-    if (event.rotationRate.gamma > 5) {
-        increaseSlider();
-    } else if (event.rotationRate.gamma < -5) {
-        decreaseSlider();
-    }
+    applyChange(event.rotationRate.gamma, 5);
 }
 
 function handleMotionCanBeDisabled(event) {
-    if (event.rotationRate.gamma > 5) {
-        increaseSlider();
-    } else if (event.rotationRate.gamma < -5) {
-        decreaseSlider();
+    const disableMotion = document.getElementById('disableMotion');
+    if (!disableMotion.checked) {
+        applyChange(event.rotationRate.gamma, 5);
     }
 }

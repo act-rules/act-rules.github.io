4a80ebad5d5645a87f2cd9d081036cb14d12b8a9
jkodu <jey.nandakumar@gmail.com>
2021-01-25 09:42:38 +0000

    chore: update website

---
+++ component---src-pages-index-js-9f5987c4172e13e0921a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{RXBc:function(e,n,t){"use strict";t.r(n);var o=t("q1tI"),a=t.n(o),c=t("YwZP");n.default=function(){return a.a.createElement(c.Redirect,{noThrow:!0,to:"/pages/about"})}}}]);
+//# sourceMappingURL=component---src-pages-index-js-9f5987c4172e13e0921a.js.map

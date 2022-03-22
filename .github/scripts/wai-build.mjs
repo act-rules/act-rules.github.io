a12a859ee3b19e0fe65864abe22a18b5d5021671
Wilco Fiers <wilco.fiers@deque.com>
2022-03-22 13:31:31 +0100

    Add wai-deploy workflow

--- .github/scripts/wai-build.mjs
+++ .github/scripts/wai-build.mjs
@@ -1,27 +1,24 @@
 #!/usr/bin/env zx
 import 'zx/globals';
 
-const tmpDir = `wcag-act-rules-tmp`;
-const rulesDir = `./_rules/`;
-const glossaryDir = `./pages/glossary/`;
-const testAssetsDir = `./test-assets/`;
+const config = {
+  tmpDir: `./wcag-act-rules-tmp/`,
+  rulesDir: `./_rules/`,
+  glossaryDir: `./pages/glossary/`,
+  testAssetsDir: `./test-assets/`,
+}
 
-await cloneWcagActRules({ tmpDir });
-await generateProposedRulePages({
-  tmpDir, rulesDir, glossaryDir
-});;
-await generateTestCases({
-  tmpDir, rulesDir, testAssetsDir
-});
-
-await commitAndPush({ tmpDir });
+await cloneWcagActRules(config);
+await generateProposedRulePages(config);
+await generateTestCases(config);
+await commitAndPush(config);
 
 
-async function cloneWcagActRules ({ tmpDir }) {
-  await $`rm -rf ./${tmpDir}`;
+async function cloneWcagActRules({ tmpDir }) {
+  await $`rm -rf ${tmpDir}`;
   await $`git clone \
     --single-branch \
-    --branch automation-test \
+    --branch master \
     git@github.com:w3c/wcag-act-rules.git ${tmpDir} \
     --depth 1
   `;
@@ -31,7 +28,7 @@ async function generateProposedRulePages({ tmpDir, rulesDir, glossaryDir }) {
   await $`node ./node_modules/act-tools/dist/cli/rule-transform.js \
   --rulesDir "${rulesDir}" \
   --glossaryDir "${glossaryDir}" \
-  --outDir "./${tmpDir}/" \
+  --outDir "${tmpDir}" \
   --proposed \
   --matrix
   `;
@@ -41,13 +38,14 @@ async function generateTestCases({ tmpDir, rulesDir, testAssetsDir }) {
   await $`node ./node_modules/act-tools/dist/cli/build-examples.js \
     --rulesDir "${rulesDir}" \
     --testAssetsDir "${testAssetsDir}" \
-    --outDir "./${tmpDir}/"
+    --outDir "${tmpDir}" \
+    --proposed
   `;
 }
 
 async function commitAndPush({ tmpDir }) {
   const commitMessage = (await $`git log -1 --pretty=%B`).stdout
-  cd(`./${tmpDir}`);
+  cd(tmpDir);
   await $`git add .`;
   await $`git commit -m ${commitMessage}`;
   await $`git push`;

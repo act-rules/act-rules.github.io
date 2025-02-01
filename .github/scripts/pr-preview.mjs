b61c565ce0dc6d9bb766e82b4bde46e34849c889
Daniel <dmontalvo@w3.org>
2025-02-01 12:10:29 +0100

    Add createOrCheckoutBranch

--- .github/scripts/pr-preview.mjs
+++ .github/scripts/pr-preview.mjs
@@ -1,9 +1,12 @@
 #!/usr/bin/env zx
 import 'zx/globals';
-import { config} from './commons.mjs'
+import { config, cloneWcagActRules, commitAndPush } from './commons.mjs'
 
+await cloneWcagActRules(config);
 await generateProposedRulePages(config);
 await generateTestCases(config);
+const commitMessage = (await $`git log -1 --pretty=%B`).stdout;
+await commitAndPush(config, commitMessage);
 
 async function generateProposedRulePages({ tmpDir, rulesDir, glossaryDir, testAssetsDir }) {
   await $`node ./node_modules/act-tools/dist/cli/rule-transform.js \

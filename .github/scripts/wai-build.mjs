38548c8a535f055b1965f2153f31db9790d906c7
Godwin <goodgodwin@hotmail.com>
2026-03-30 15:48:20 -0700

    Added the glossary page

--- .github/scripts/wai-build.mjs
+++ .github/scripts/wai-build.mjs
@@ -2,24 +2,36 @@
 import 'zx/globals';
 import { config, cloneWcagActRules, commitAndPush } from './commons.mjs'
 
+const ACT_TOOLS_PATH = process.env.ACT_TOOLS_PATH || './node_modules/act-tools';
+
 await cloneWcagActRules(config);
 await generateProposedRulePages(config);
 await generateexamples(config);
+await generateGlossaryPage({ ...config, wcagActRulesDir: config.tmpDir });
 const commitMessage = (await $`git log -1 --pretty=%B`).stdout;
 await commitAndPush(config, commitMessage);
 
+async function generateGlossaryPage({ tmpDir, rulesDir, glossaryDir, testAssetsDir }) {
+  await $`node ${ACT_TOOLS_PATH}/dist/cli/generate-glossary.js \
+    --rulesDir "${rulesDir}" \
+    --glossaryDir "${glossaryDir}" \
+    --testAssetsDir "${testAssetsDir}" \
+    --outDir "${tmpDir}"
+  `;
+}
+
 async function generateProposedRulePages({ tmpDir, rulesDir, glossaryDir, testAssetsDir }) {
-  await $`node ./node_modules/act-tools/dist/cli/rule-transform.js \
-  --rulesDir "${rulesDir}" \
-  --glossaryDir "${glossaryDir}" \
-  --testAssetsDir "${testAssetsDir}" \
-  --outDir "${tmpDir}" \
-  --proposed
+  await $`node ${ACT_TOOLS_PATH}/dist/cli/rule-transform.js \
+    --rulesDir "${rulesDir}" \
+    --glossaryDir "${glossaryDir}" \
+    --testAssetsDir "${testAssetsDir}" \
+    --outDir "${tmpDir}" \
+    --proposed
   `;
 }
 
 async function generateexamples({ tmpDir, rulesDir, testAssetsDir }) {
-  await $`node ./node_modules/act-tools/dist/cli/build-examples.js \
+  await $`node ${ACT_TOOLS_PATH}/dist/cli/build-examples.js \
     --rulesDir "${rulesDir}" \
     --testAssetsDir "${testAssetsDir}" \
     --outDir "${tmpDir}" \
